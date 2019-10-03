dd9d00c042573d9129d2d12eea4d17c4572b10cb
gerwant <alekseytyurinspb@gmail.com>
2019-10-04 02:14:12 +0300

    naive multipattern

--- src/main.cu
+++ src/main.cu
@@ -177,6 +177,103 @@ void match_pe(std::string pattern, std::string subject_string_filename) {
 }
 
 
+void match_pe_pointer(std::string pattern, std::string subject_string_filename) {
+    
+    if (pattern.size() > 31) {
+        std::cout << "pattern should be less then or eq 31 bytes\n";
+        return;
+    }
+    auto pattern_size = pattern.size();
+    // pattern.resize(31,'0'); 
+        
+    std::string dummy_fun;
+
+    //maybe asyncronous read from disk and jit;
+    dummy_fun += "extern fn dummy(text : &[u8], text_size : i32, result_buf : &mut[i32]) -> (){\n";
+
+    dummy_fun += "  string_match_pseudoKMP_pointer( \"" + pattern + "\", "
+              + std::to_string(pattern_size) + ",text, text_size,result_buf,256,256)}"; //;
+
+    std::string program = std::string((char*)fun_impala) + dummy_fun;
+
+    std::cout << "compiling ... " << std::flush;
+    am::timer time;
+    // time.start();
+
+    auto key = anydsl_compile(program.c_str(),program.size(),0);
+    typedef void (*function) (const char*, int, const int *);
+    auto call = reinterpret_cast<function>(anydsl_lookup_function(key,"dummy"));
+    
+    if (call == nullptr) {
+        std::cout << "compiliacion failed\n";
+        return;
+    } else {
+        std::cout << "succesfully compiled\n";
+    }
+
+    // time.stop();
+    // std::cout << "compilation time " << time.milliseconds() << " ms" << std::endl;
+    
+
+    auto text_size = GetFileSize(subject_string_filename) - 1;//TODO
+    
+    //read file
+    FILE *f = fopen(subject_string_filename.c_str(), "rb");
+    // fseek(f, 0, SEEK_END);
+    // long fsize = ftell(f);
+    // fseek(f, 0, SEEK_SET);  /* same as rewind(f); */
+
+    char *subject_string = new char[text_size];
+    fread(subject_string, 1, text_size, f);
+    fclose(f);
+
+    // string[fsize] = 0;
+    // int fdin,fdout;
+    // if (fdin = open(subject_string_filename.c_str(),O_RDONLY) < 0) {
+        // std::cout << "can't open file" << subject_string_filename << "\n";
+        // return;
+    // }
+    // char *subject_string = new char[text_size];
+    // read(fdin,(void*)subject_string,text_size);
+    
+    std::cout << "\n";
+    int* result_buf = new int[text_size];
+    int* dresult_buf;
+    char* dtext;
+    //think about data transfer;
+    cudaMalloc((void**)&dtext, text_size * sizeof(char));
+    cudaMemcpy((void*)dtext,subject_string,text_size * sizeof(char),cudaMemcpyHostToDevice);
+    delete[](subject_string);
+    cudaMalloc((void**)&dresult_buf, text_size * sizeof(int));
+    
+    // for(int i = 0; i < text_size; i++) {
+        // result_buf[i] = -1;
+    // }
+    // cudaMemset((void*)dresult_buf, -1, text_size*sizeof(int));
+    
+    // call(text.c_str(),text_size,result_buf);
+    // time.reset();
+    std::cout << "running ... " << "\n";
+    time.start();
+    
+    call(dtext,text_size,dresult_buf);
+
+    time.stop();
+    std::cout << "running time " << time.milliseconds() << " ms" << std::endl;
+
+    cudaMemcpy((void*)result_buf,dresult_buf,text_size*sizeof(int),cudaMemcpyDeviceToHost);
+
+    for (int i = 0; i < text_size; i++) {
+        std::cout << result_buf[i];
+    }
+    std::cout << "\n";
+    
+    cudaFree(dresult_buf);
+    cudaFree(dtext);
+    delete[] (result_buf);
+}
+
+
 
 int main(int argc, char** argv) {
     
@@ -188,7 +285,7 @@ int main(int argc, char** argv) {
     std::string subject = std::string("data/subject.txt");
     
     // match_naive_cuda(pattern,text);
-    match_pe(pattern,subject);
+    match_pe_pointer(pattern,subject);
     // std::cout << GetFileSize(std::string("subject.txt"));
 
     return 0;
--- src/test.cu
+++ src/test.cu
@@ -1,4 +1,5 @@
 #include <string>
+#include <vector>
 #include <iostream>
 #include <stdlib.h>
 #include <sys/mman.h>
@@ -35,6 +36,33 @@ __global__ void match(char* pattern, int pattern_size, char* text, int text_size
     }
 }
 
+__global__ void match_multy(char* pattern, int* p_sizes, int p_number, char* text, int text_size, int* result_buf) {
+
+    int t_id = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if(t_id < text_size){
+        int p_offset = 0;
+        int matched = 0;
+        result_buf[t_id] = -1;
+
+        for(int i = 0; i < p_number; i++) {
+            matched = 0;
+            for(int j = 0; j < p_sizes[i]; j++){
+                
+                if(text[t_id + j] != pattern[j+p_offset]) {
+                    matched = -1;
+                }
+            }
+
+            p_offset += p_sizes[i]; 
+            
+            if(matched != -1) {
+                result_buf[t_id] = i;
+            }
+        }             
+    }
+}
+
 __global__ void match_struct(Template pattern, char* text, int text_size, int* result_buf) {
 
     int t_id = blockIdx.x * blockDim.x + threadIdx.x;
@@ -63,40 +91,45 @@ long GetFileSize(std::string filename)
     return rc == 0 ? stat_buf.st_size : -1;
 }
 
+void multipattern_match(int p_number,char** argv_patterns, char* file_name){
 
-int main(int argc, char** argv) {
-
-    std::string pattern = std::string(argv[1]);
-
-    if (pattern.size() > 31) {
-        std::cout << "pattern should be less then or eq 31 bytes\n";
-        return 0;
+    int* sizes = new int[p_number];
+    int len = 0;
+    for(int i = 1; i < p_number+1; i++) {
+        auto str = std::string(argv_patterns[i]);
+        sizes[i-1] = str.length();
+        len += str.length();    
     }
-    auto pattern_size = pattern.size();
-    pattern.resize(31,'0');
-    Template dpattern_s;
-    dpattern_s.size = pattern_size;
-    for(int i = 0; i < pattern_size; i++) {
-        dpattern_s.array[i] = pattern[i];
-    }
-    // char* dpattern;
-    // cudaMalloc((void**)&dpattern, pattern_size * sizeof(char));
-    // cudaMemcpy((void*)dpattern,pattern.c_str(),pattern_size,cudaMemcpyHostToDevice); 
-    std::cout << dpattern_s.size << "\n";
-    for(int i = 0; i < pattern_size; i++) {
-        std::cout << dpattern_s.array[i] << "\n";
-    }
-    // std::cout << dpatt << "\n";
 
-    std::string subject_string_filename("data/subject.txt");
+    char* patterns = new char[len];
+    
+    int offset = 0;
+
+    for(int i = 0; i < p_number; i++){
+
+        for(int j = 0; j < sizes[i]; j++){
+            patterns[offset+j] = argv_patterns[i+1][j];
+        }
+        offset+=sizes[i];    
+    }
+
+    char* dpatterns;
+    int* dsizes;
+    cudaMalloc((void**)&dsizes, (p_number)*sizeof(int));
+    cudaMemcpy((void*)dsizes, sizes, (p_number)*sizeof(int), cudaMemcpyHostToDevice); 
+    cudaMalloc((void**)&dpatterns, len * sizeof(char));
+    cudaMemcpy((void*)dpatterns, patterns, len*sizeof(char), cudaMemcpyHostToDevice);
+
+    std::string subject_string_filename(file_name);
 
     auto text_size = GetFileSize(subject_string_filename) - 1;//TODO
     
     //read file
-    FILE *f = fopen(subject_string_filename.c_str(), "rb");
-    // fseek(f, 0, SEEK_END);
-    // long fsize = ftell(f);
-    // fseek(f, 0, SEEK_SET);  /* same as rewind(f); */
+    FILE *f;
+    if((f = fopen(subject_string_filename.c_str(), "rb")) == NULL){
+	std::cout << "can not oppen file" << subject_string_filename << "\n";
+	    return;
+    }
 
     char *subject_string = new char[text_size];
     fread(subject_string, 1, text_size, f);
@@ -109,7 +142,100 @@ int main(int argc, char** argv) {
     //think about data transfer;
     cudaMalloc((void**)&textptr, text_size * sizeof(char));
     cudaMemcpy((void*)textptr,subject_string,text_size,cudaMemcpyHostToDevice);
+    cudaMalloc((void**)&dresult_buf, text_size * sizeof(int));
+    
+    dim3 block(1024);
+    int grid_size = (text_size + block.x - 1) / block.x;
+    dim3 grid(grid_size);
+
+    // cudaEvent_t start, stop;
+    // cudaEventCreate(&start);
+    // cudaEventCreate(&stop);
+    
+    std::cout << "running ..." << "\n";
+
+    am::timer time;
+    time.start();
+    match_multy<<<grid,block>>>(dpatterns,dsizes,p_number,textptr,text_size,dresult_buf);
+    // cudaEventRecord(stop);
+    delete[](sizes);
+    delete[](patterns);
     delete[](subject_string);
+    cudaDeviceSynchronize();
+    time.stop();
+    std::cout << "running time " << time.milliseconds() << " ms" << std::endl;
+    cudaMemcpy((void*)result_buf,dresult_buf,text_size*sizeof(int),cudaMemcpyDeviceToHost);
+    // cudaEventSynchronize(stop);
+    cudaDeviceSynchronize();
+
+    float milliseconds = 0;
+    // cudaEventElapsedTime(&milliseconds, start, stop);
+
+    // std::cout << "running time " << milliseconds << "ms" << "\n";
+
+
+
+    for (int i = 0; i < text_size; i++) {
+        std::cout << result_buf[i];
+    }
+    std::cout << "\n";
+    
+    cudaFree(dresult_buf);
+    cudaFree(textptr);
+    // cudaFree(dpattern);
+    // cudaEventDestroy(start);
+    // cudaEventDestroy(stop);
+    delete[] (result_buf);  
+
+}
+
+
+int main(int argc, char** argv) {
+
+    
+    // std::string pattern = std::string(argv[1]);
+
+    // if (pattern.size() > 31) {
+        // std::cout << "pattern should be less then or eq 31 bytes\n";
+        // return 0;
+    // }
+    // auto pattern_size = pattern.size();
+    // pattern.resize(31,'0');
+    // Template dpattern_s;
+    // dpattern_s.size = pattern_size;
+    // for(int i = 0; i < pattern_size; i++) {
+        // dpattern_s.array[i] = pattern[i];
+    // }
+    // char* dpatterns;
+    // int* dsizes;
+    // cudaMalloc((void**)&dsizes, (argc-1)*sizeof(int));
+    // cudaMemcpy((void*)dsizes, sizes, (argc-1)*sizeof(int), cudaMemcpyHostToDevice); 
+    // cudaMalloc((void**)&dpatterns, len * sizeof(char));
+    // cudaMemcpy((void*)dpatterns, patterns, len*sizeof(char), cudaMemcpyHostToDevice); 
+    
+
+    /*std::string subject_string_filename("data/subject.txt");
+
+    auto text_size = GetFileSize(subject_string_filename) - 1;//TODO
+    
+    //read file
+    FILE *f;
+    if((f = fopen(subject_string_filename.c_str(), "rb")) == NULL){
+	std::cout << "can not oppen file" << subject_string_filename << "\n";
+	return 0;
+    }
+
+    char *subject_string = new char[text_size];
+    fread(subject_string, 1, text_size, f);
+    fclose(f);
+    // std::cin >> text;
+    int* result_buf = new int[text_size];
+    int* dresult_buf;
+    // std::cout << "text length : " << text_size << "\n";
+    char* textptr;
+    //think about data transfer;
+    cudaMalloc((void**)&textptr, text_size * sizeof(char));
+    cudaMemcpy((void*)textptr,subject_string,text_size,cudaMemcpyHostToDevice);
     cudaMalloc((void**)&dresult_buf, text_size * sizeof(int));
     
     dim3 block(1024);
@@ -125,9 +251,13 @@ int main(int argc, char** argv) {
     am::timer time;
     time.start();
     // cudaEventRecord(start);
-    match_struct<<<grid,block>>>(dpattern_s,textptr,text_size,dresult_buf);
+    // match_struct<<<grid,block>>>(dpattern_s,textptr,text_size,dresult_buf);
     // match<<<grid,block>>>(dpattern,pattern_size,textptr,text_size,dresult_buf);
+    match_multy<<<grid,block>>>(dpatterns,dsizes,argc-1,textptr,text_size,dresult_buf);
     // cudaEventRecord(stop);
+    delete[](sizes);
+    delete[](patterns);
+    delete[](subject_string);
     cudaDeviceSynchronize();
     time.stop();
     std::cout << "running time " << time.milliseconds() << " ms" << std::endl;
@@ -152,7 +282,9 @@ int main(int argc, char** argv) {
     // cudaFree(dpattern);
     cudaEventDestroy(start);
     cudaEventDestroy(stop);
-    delete[] (result_buf);
+    delete[] (result_buf);*/
+
+    multipattern_match(argc-1,argv,"data/subject.txt");
 
     return 0;
 }
